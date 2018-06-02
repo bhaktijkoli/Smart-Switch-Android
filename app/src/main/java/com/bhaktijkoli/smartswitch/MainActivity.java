f94384311f3e9ea12c9b3954b5c7875ba962ce3b
Bhaktij Koli <bhaktijkoli121@gmail.com>
2018-06-03 03:49:28 +0530

    Added Network service discovery

--- app/src/main/java/com/bhaktijkoli/smartswitch/MainActivity.java
+++ app/src/main/java/com/bhaktijkoli/smartswitch/MainActivity.java
@@ -2,12 +2,38 @@ package com.bhaktijkoli.smartswitch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.bhaktijkoli.smartswitch.utils.NsdUtils;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private NsdUtils mNsdUtils;
+    private String ipaddress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        NsdUtils mNsdUtils = new NsdUtils(this);
+        mNsdUtils.setNsdUtilListner(new NsdUtils.NsdUtilListner() {
+            @Override
+            public void onScanStart() {
+
+            }
+
+            @Override
+            public void onScanStop() {
+
+            }
+
+            @Override
+            public void onDeviceFound(String ipaddress) {
+                MainActivity.this.ipaddress = ipaddress;
+                Toast.makeText(MainActivity.this, "IP: " + ipaddress, Toast.LENGTH_LONG).show();
+            }
+        });
+        mNsdUtils.scan();
     }
 }
